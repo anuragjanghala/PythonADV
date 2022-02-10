5ff03823c222b75c827d00067d66d3842ec8000d
anuragjanghala <42205669+anuragjanghala@users.noreply.github.com>
2022-02-10 17:42:32 +0000

    worked on class, instances and constructor in oops

---
+++ OOPS/main.py
@@ -0,0 +1,75 @@
+class Item:
+    pay_rate = 0.8 # The pay rate after 20% discount
+    all = []
+    def __init__(self, name: str, price: float, quantity = 0):
+        #print(f'an instance created: {name}')
+        
+        # Run validations to the recieved arguments
+        assert price >= 0, f"Price {price} is not greater than or equal to zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater than or equal to zero!"
+        
+        # Assign to self object
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        
+        
+        # Actions to execute
+        Item.all.append(self)
+    
+    def calculate_total_price(self):
+        return self.price * self.quantity  #x * y
+    
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
+        
+    def __repr__(self):
+        return f"Item('{self.name}, {self.price}, {self.quantity}')"
+        
+
+item1 = Item("Phone", 100, 1)
+item2 = Item("Laptop", 1000, 3)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
+
+print(Item.all)
+
+for instance in Item.all:
+    print(instance.name)
+
+
+# item1 = Item("Phone",100,1)
+# item1.name = "Phone"
+# item1.price = 100
+# item1.quantity = 5
+# print(item1.name, item1.price, item1.quantity)
+# print(item1.calculate_total_price())
+
+
+# item2 = Item("Laptop",1000,3)
+# item2.name = "Laptop"
+# item2.price = 1000
+# item2.quantity = 3
+# print(item2.name, item2.price, item2.quantity)
+# print(item2.calculate_total_price())
+
+
+# print(Item.__dict__) # All the attributes for class level
+# print(item1.__dict__) # All the attributes for instance level
+
+
+# item1.apply_discount()
+# print(item1.price)
+
+
+# item2.pay_rate = 0.7
+# item2.apply_discount()
+# print(item2.price)
+
+
+# print(type(item1))
+# print(type(item1.name))
+# print(type(item1.price))
+# print(type(item1.quantity))
+# print(item1.name.upper())
