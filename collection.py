e15a4fd971ce5ca11e5c65155b36ab146b104ba7
anuragjanghala <42205669+anuragjanghala@users.noreply.github.com>
2022-02-19 01:12:26 +0000

    collections - defaultdict

--- collection.py
+++ collection.py
@@ -29,22 +29,55 @@
 
 
 #####################################################################
-from collections import OrderedDict
+# from collections import OrderedDict
 
-ordered_dict = OrderedDict()
+# ordered_dict = OrderedDict()
 
-ordered_dict['a'] = 1
-ordered_dict['b'] = 2
-ordered_dict['c'] = 3
-ordered_dict['d'] = 4
+# ordered_dict['a'] = 1
+# ordered_dict['b'] = 2
+# ordered_dict['c'] = 3
+# ordered_dict['d'] = 4
 
-print(ordered_dict)
+# print(ordered_dict)
 
-ordered_dict = {} # nowadays this also remember order
+# ordered_dict = {} # nowadays this also remember order
 
-ordered_dict['a'] = 1
-ordered_dict['b'] = 2
-ordered_dict['c'] = 3
-ordered_dict['d'] = 4
+# ordered_dict['a'] = 1
+# ordered_dict['b'] = 2
+# ordered_dict['c'] = 3
+# ordered_dict['d'] = 4
 
-print(ordered_dict)
+# print(ordered_dict)
+
+
+######################################################################
+from collections import defaultdict
+
+default_dict = defaultdict(int)
+
+default_dict['a'] = 1
+default_dict['b'] = 2
+
+print(default_dict)
+print(default_dict['a'])
+print(default_dict[0])
+
+
+default_dict = defaultdict(list)
+
+default_dict['a'] = 1
+default_dict['b'] = 2
+
+print(default_dict)
+print(default_dict['a'])
+print(default_dict[0])
+
+
+default_dict = {} # in this case normal dictionary
+
+default_dict['a'] = 1
+default_dict['b'] = 2
+
+print(default_dict)
+print(default_dict['a'])
+print(default_dict[0]) # this will raise key error
