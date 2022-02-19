6173a0cc8629007c9373657c8fcab1035afbb045
anuragjanghala <42205669+anuragjanghala@users.noreply.github.com>
2022-02-19 01:02:40 +0000

    collections - namedtuple

--- collection.py
+++ collection.py
@@ -1,17 +1,28 @@
 # collections: Counter, namedtuple, OrderDict, defaultdict, deque
-from collections import Counter
+# from collections import Counter
  
-a = 'aaaaaaabbbbbbbbbcccccccccc'
-my_counter = Counter(a)
-print(my_counter)
+# a = 'aaaaaaabbbbbbbbbcccccccccc'
+# my_counter = Counter(a)
+# print(my_counter)
 
-print(my_counter.items())
-print(my_counter.keys())
-print(my_counter.values())
+# print(my_counter.items())
+# print(my_counter.keys())
+# print(my_counter.values())
 
-print(my_counter.most_common(2))
-print(my_counter.most_common(2)[0])
-print(my_counter.most_common(2)[0][0])
+# print(my_counter.most_common(2))
+# print(my_counter.most_common(2)[0])
+# print(my_counter.most_common(2)[0][0])
 
-print(my_counter.elements())
-print(list(my_counter.elements()))
+# print(my_counter.elements())
+# print(list(my_counter.elements()))
+
+
+####################################################################
+from collections import namedtuple
+
+Point = namedtuple('Point','x,y')
+
+pt = Point(1, -4)
+
+print(pt)
+print(pt.x, pt.y)
