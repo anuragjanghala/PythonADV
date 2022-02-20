f1d5f200dead583fe8f97bbb50987857847660be
anuragjanghala <42205669+anuragjanghala@users.noreply.github.com>
2022-02-20 23:58:23 +0000

    itertools - permutation

--- itertools.py
+++ itertools.py
@@ -1,15 +1,26 @@
 # itertools : product, permutations, combinations, accumulate, groupby, and infinite iterators
 
-from itertools import product
+# from itertools import product
 
-a = [1,2]
-b = [3]
+# a = [1,2]
+# b = [3]
 
-prod = product(a,b) # This willfive the cartesian product of a and b
-print(prod)
-print(type(prod))
-print(list(prod))
+# prod = product(a,b) # This willfive the cartesian product of a and b
+# print(prod)
+# print(type(prod))
+# print(list(prod))
 
 
-prod = product(a,b, repeat=2) # We can also define a number of repetition
-print(list(prod))
+# prod = product(a,b, repeat=2) # We can also define a number of repetition
+# print(list(prod))
+
+######################################################################
+from itertools import permutations
+
+a = [1,2,3]
+
+perm  = permutations(a)
+print(list(perm))
+
+perm  = permutations(a, 2) # for shorter length permutation
+print(list(perm))
