87dfbdee7e75e6886fdcb79f7271878232894f47
anuragjanghala <42205669+anuragjanghala@users.noreply.github.com>
2022-02-21 00:17:47 +0000

    itertools - groupby

--- itertools.py
+++ itertools.py
@@ -41,24 +41,59 @@
 
 
 ######################################################################
-from itertools import accumulate
-import operator
+# from itertools import accumulate
+# import operator
+
+# a = [1,2,3,4]
+# acc = accumulate(a) # by default it will sum
+# print(a)
+# print(list(acc))
+
+# acc = accumulate(a, func=operator.mul) # it will do mul
+# print(a)
+# print(list(acc))
+
+# a = [1,2,5,3,4]
+# acc = accumulate(a, func=max) # will get max
+# print(a)
+# print(list(acc))
+
+# a = [2,5,3,1,4]
+# acc = accumulate(a, func=min) # will get min
+# print(a)
+# print(list(acc))
+
+
+######################################################################
+from itertools import groupby
+
+
+def smaller_than_3(x):
+    return x < 3 # will return true or false
 
 a = [1,2,3,4]
-acc = accumulate(a) # by default it will sum
-print(a)
-print(list(acc))
+group_obj = groupby(a, key=smaller_than_3)
+print(group_obj)
 
-acc = accumulate(a, func=operator.mul) # it will do mul
-print(a)
-print(list(acc))
+for key, value in group_obj:
+    print(key, list(value))
 
-a = [1,2,5,3,4]
-acc = accumulate(a, func=max) # will get max
-print(a)
-print(list(acc))
 
-a = [2,5,3,1,4]
-acc = accumulate(a, func=min) # will get min
-print(a)
-print(list(acc))
+    
+
+a = [1,2,3,4]
+group_obj = groupby(a, key=lambda x: x<3)
+print(group_obj)
+
+for key, value in group_obj:
+    print(key, list(value))
+    
+
+
+    
+persons = [{'name': 'Tim', 'age': 25}, {'name': 'Dan', 'age': 25},
+           {'name': 'Lisa', 'age': 27}, {'name': 'Claire', 'age': 28}]
+
+group_obj = groupby(persons, key= lambda x: x['age'])
+for key, value in group_obj:
+    print(key, list(value))
