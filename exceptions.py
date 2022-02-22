39b6bccc9ecb3ce75884ca303210f0daee438f4e
anuragjanghala <42205669+anuragjanghala@users.noreply.github.com>
2022-02-22 00:41:54 +0000

    Error - defining self error classes

--- exceptions.py
+++ exceptions.py
@@ -46,25 +46,52 @@
 
 # try except block
 
-try:
-    a = 5 / 0
-except:
-    print('an error happened')
+# try:
+#     a = 5 / 0
+# except:
+#     print('an error happened')
     
-try:
-    a = 5 / 0
-except Exception as e:
-    print(e)
+# try:
+#     a = 5 / 0
+# except Exception as e:
+#     print(e)
     
     
+# try:
+#     a = 5 / 0
+#     b = a + 4
+# except ZeroDivisionError as e:
+#     print(e)
+# except TypeError as e:
+#     print(e)
+# else:
+#     print('everything is fine')
+# finally:
+#     print('cleaning up...')
+
+
+#######################################################################
+# defining errors on our own
+class ValueTooHighError(Exception):
+    pass
+
+
+class ValueTooSmallError(Exception):
+    def __init__(self, message, value):
+        self.message = message
+        self.value = value
+
+def test_value(x):
+    if x > 100:
+        raise ValueTooHighError('value is too high')
+    if x < 5:
+        raise ValueTooSmallError('value is too small', x)
+
+# test_value(200)
+
 try:
-    a = 5 / 0
-    b = a + 4
-except ZeroDivisionError as e:
+    test_value(1)
+except ValueTooHighError as e:
     print(e)
-except TypeError as e:
-    print(e)
-else:
-    print('everything is fine')
-finally:
-    print('cleaning up...')
+except ValueTooSmallError as e:
+    print(e.message, e.value)
