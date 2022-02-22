c4f615d2429c65ce252a6c239953afc437963ef7
anuragjanghala <42205669+anuragjanghala@users.noreply.github.com>
2022-02-22 00:20:12 +0000

    added Errors file

---
+++ exceptions.py
@@ -0,0 +1,33 @@
+# Errors and exceptions
+# syntax error
+# a = 5 print(a)
+# print(a))
+
+# exception type error
+# a = 5 + '10'
+
+# import error
+# import somemodule
+
+# name error
+# a = 5
+# b = c
+
+# file not found error
+# f = open('somefile.txt')
+
+
+# value error
+# a = [1,2,3]
+# a.remove(4)
+# print(a)
+
+
+# index error
+# a = [1,2,3]
+# a[4]
+
+
+# key error
+# my_dict = {'name': 'max'}
+# my_dict['age']
