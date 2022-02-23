f1623573e60b75aab33c3b2288f7bf286594010a
anuragjanghala <42205669+anuragjanghala@users.noreply.github.com>
2022-02-23 04:04:52 +0000

    from json file to normal python dict convertion

--- json_tutorial.py
+++ json_tutorial.py
@@ -2,11 +2,23 @@ import json
 
 person = {"name": "John", "age": 30, "city": "New York", "titles": ["engineer", "programmer"], "haschildren": False}
 
-personJSON = json.dumps(person)
-print(personJSON)
+# personJSON = json.dumps(person)
+# print(personJSON)
 
-personJSON = json.dumps(person, indent=4, separators=('; ', '= '), sort_keys=True)
-print(personJSON)
+# personJSON = json.dumps(person, indent=4, separators=('; ', '= '), sort_keys=True)
+# print(personJSON)
 
-with open('person.json', 'w') as file:
-    json.dump(person, file, indent=4)
+# with open('person.json', 'w') as file:
+#     json.dump(person, file, indent=4)
+
+###################################################################
+personJSON = json.dumps(person, indent=4, sort_keys=True)
+
+person = json.loads(personJSON)
+print(person)
+
+# from json to python with json file
+
+with open('person.json') as file:
+    person = json.load(file)
+    print(person)
