4bf17cefd626ebd522bb2b69fbeb178e2693503b
anuragjanghala <42205669+anuragjanghala@users.noreply.github.com>
2022-02-23 03:27:40 +0000

    logging - TimeRotatingFileHandler (after 5 sec new log file created)

--- logging_helper.py
+++ logging_helper.py
@@ -68,16 +68,33 @@
 
 
 ##################################################################
+# import logging
+# from logging.handlers import RotatingFileHandler
+
+# logger = logging.getLogger(__name__)
+# logger.setLevel(logging.INFO)
+
+# # roll over after 2kb, and keep backup logs app.log.1, app.log.2, etc
+
+# handler = RotatingFileHandler('app.log', maxBytes=2000, backupCount=5)
+# logger.addHandler(handler)
+
+# for _ in range(10000):
+#     logger.info('hello, world!')
+
+
+###################################################################
 import logging
-from logging.handlers import RotatingFileHandler
+import time
+from logging.handlers import TimedRotatingFileHandler
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-# roll over after 2kb, and keep backup logs app.log.1, app.log.2, etc
-
-handler = RotatingFileHandler('app.log', maxBytes=2000, backupCount=5)
+# s, m, h, d, midnight, w0, w1
+handler = TimedRotatingFileHandler('timed_test.log', when='s', interval=5, backupCount=5)
 logger.addHandler(handler)
 
-for _ in range(10000):
-    logger.info('hello, world!')
+for _ in range(6):
+    logger.info('Hello, World!')
+    time.sleep(5)
