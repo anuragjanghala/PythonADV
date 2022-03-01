135cb7e2a29c1f949ca5eae3b3b76ebecb3b5390
anuragjanghala <42205669+anuragjanghala@users.noreply.github.com>
2022-03-01 15:13:20 +0000

    decorators with arguments

--- decorators.py
+++ decorators.py
@@ -4,50 +4,71 @@
 
 # it allows to add new functionality without changing existing one
 
+from ast import arg
 import functools
 
 # @mydecorator
 # def dosomething():
 #     pass
 
-def start_end_decorator(func):
+# def start_end_decorator(func):
     
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        print('start')
-        res = func(*args, **kwargs)
-        print('end')
-        return res
-    return wrapper 
+#     @functools.wraps(func)
+#     def wrapper(*args, **kwargs):
+#         print('start')
+#         res = func(*args, **kwargs)
+#         print('end')
+#         return res
+#     return wrapper 
 
-def print_name():
-    print("alex")
+# def print_name():
+#     print("alex")
     
-print_name()
+# print_name()
     
-print_name = start_end_decorator(print_name)
+# print_name = start_end_decorator(print_name)
 
-print_name()
-
-
-@start_end_decorator
-def print_name():
-    print("alex")
-
-# we dont need this line
-# print_name = start_end_decorator(print_name)    
-print_name()
+# print_name()
 
 
-@start_end_decorator
-def add5(x):
-    return x + 5
+# @start_end_decorator
+# def print_name():
+#     print("alex")
 
-res = add5(10)
-print(res)
-
-
-print(help(add5))
-print(add5.__name__) # python got confused as both identities have same name to fix this we functools
+# # we dont need this line
+# # print_name = start_end_decorator(print_name)    
+# print_name()
 
 
+# @start_end_decorator
+# def add5(x):
+#     return x + 5
+
+# res = add5(10)
+# print(res)
+
+
+# print(help(add5))
+# print(add5.__name__) # python got confused as both identities have same name to fix this we functools
+
+
+# Decorators with arguments
+
+def repeat(num_times):
+    
+    def decorator_repeat(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            for _ in range(num_times):
+                result = func(*args, **kwargs)
+            return result
+        return wrapper
+    return decorator_repeat
+                
+
+@repeat(num_times=5)
+def greet(name):
+    print(f'Hello {name}')
+    
+
+greet("John")
