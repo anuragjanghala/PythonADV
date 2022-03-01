f3fe26dc9eb4e4d8d0a62a14e5849d06118c8528
anuragjanghala <42205669+anuragjanghala@users.noreply.github.com>
2022-03-01 14:14:56 +0000

    random numbers - with list

--- random_examples.py
+++ random_examples.py
@@ -1,16 +1,32 @@
 import random
 
-a = random.random()
-print(a) # random float from range 0 to 1
+# a = random.random()
+# print(a) # random float from range 0 to 1
 
-a = random.uniform(1, 10)
-print(a) # float range from 1 to 10
+# a = random.uniform(1, 10)
+# print(a) # float range from 1 to 10
 
-a = random.randint(1, 10)
-print(a) # integer range 1 to 10
+# a = random.randint(1, 10)
+# print(a) # integer range 1 to 10
 
-a = random.randrange(1, 10)
-print(a) # from 1 to 10 upper bound not included
+# a = random.randrange(1, 10)
+# print(a) # from 1 to 10 upper bound not included
 
-a = random.normalvariate(0, 1)
-print(a) # it will pick random normal of mean of 0 and standard deviation of 1
+# a = random.normalvariate(0, 1)
+# print(a) # it will pick random normal of mean of 0 and standard deviation of 1
+
+#################################################################
+
+mylist = list('ABCDEFGH')
+print(mylist)
+a = random.choice(mylist)
+print(a)
+
+a = random.sample(mylist, 3)
+print(a) # this wont pick element twice
+
+a = random.choices(mylist, k=3)
+print(a) # this can pick element multiple times
+
+random.shuffle(mylist)
+print(mylist) # elements will be shuffled
