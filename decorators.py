9e1dedd3ea5a183391460b3502ac356c4ad93226
anuragjanghala <42205669+anuragjanghala@users.noreply.github.com>
2022-03-01 16:01:15 +0000

    decorators - class based

--- decorators.py
+++ decorators.py
@@ -4,9 +4,9 @@
 
 # it allows to add new functionality without changing existing one
 
-from ast import arg
+
 import functools
-import re
+
 
 # @mydecorator
 # def dosomething():
@@ -77,34 +77,66 @@ import re
 #################################################################
 # Nested Decorators
 
-def start_end_decorator(func):
+# def start_end_decorator(func):
     
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        print('start')
-        res = func(*args, **kwargs)
-        print('end')
-        return res
-    return wrapper
+#     @functools.wraps(func)
+#     def wrapper(*args, **kwargs):
+#         print('start')
+#         res = func(*args, **kwargs)
+#         print('end')
+#         return res
+#     return wrapper
 
-def debug_decorator(func):
+# def debug_decorator(func):
     
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        args_repr = [repr(a) for a in args]
-        kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
-        signature = ", ".join(args_repr + kwargs_repr)
-        print(f"Calling {func.__name__}({signature})")
-        result = func(*args, **kwargs)
-        print(f"{func.__name__!r} returned {result!r}")
-        return result
-    return wrapper
+#     @functools.wraps(func)
+#     def wrapper(*args, **kwargs):
+#         args_repr = [repr(a) for a in args]
+#         kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
+#         signature = ", ".join(args_repr + kwargs_repr)
+#         print(f"Calling {func.__name__}({signature})")
+#         result = func(*args, **kwargs)
+#         print(f"{func.__name__!r} returned {result!r}")
+#         return result
+#     return wrapper
 
-@debug_decorator
-@start_end_decorator
-def say_hello(name):
-    greeting = f'Hello, {name}'
-    print(greeting)
-    return greeting
+# @debug_decorator
+# @start_end_decorator
+# def say_hello(name):
+#     greeting = f'Hello, {name}'
+#     print(greeting)
+#     return greeting
 
-say_hello('Alex')
+# say_hello('Alex')
+
+
+#################################################################
+
+# Class Decorators : they are typically used when we wanted to maintain and update a state
+
+# when we decorate a function with a class, that function becomes an instance of the class.
+
+# When we decorate a function with a class, the function is automatically passed as the first argument to the init constructor.
+
+class CountCalls:
+    def __init__(self, func):
+        self.func = func
+        self.num_calls = 0
+    
+    def __call__(self, *args, **kwargs):
+        self.num_calls += 1
+        print(f'this is executed {self.num_calls} times')
+        return self.func(*args, **kwargs)
+
+# cc = CountCalls(None)
+# cc()
+
+# classes that decorate functions can either accept arguments or fall back to a default if no argument is passed. 
+# here we didn't use argument while calling decorator
+
+@CountCalls
+def say_hello():
+    print('hello')
+    
+say_hello()
+say_hello()
