90a698cdb17d72d0bf73fa6f494ce3005f210deb
anuragjanghala <42205669+anuragjanghala@users.noreply.github.com>
2022-03-06 20:14:10 +0000

    multithreading

--- multi.py
+++ multi.py
@@ -1,33 +1,64 @@
-from multiprocessing import Process
-from multiprocessing.dummy import freeze_support
-import os
-import time
+# from multiprocessing import Process
+# from multiprocessing.dummy import freeze_support
+# import os
+# import time
 
+
+# def square_numbers():
+#     for i in range(100):
+#         i*i
+#         time.sleep(0.1)
+
+# processes = []
+# num_processes = os.cpu_count()
+# # print(num_processes)
+
+# # create processes
+# for i in range(num_processes):
+#     p = Process(target=square_numbers)
+#     processes.append(p)
+
+# # start processes
+# if __name__ == '__main__':
+#     freeze_support()
+#     for p in processes:
+#         p.start() 
+    
+#     # join
+#     for p in processes:
+#         p.join()
+    
+
+    
+# print('end main')
+
+
+##################################################################
+from threading import Thread
+import time
 
 def square_numbers():
     for i in range(100):
         i*i
         time.sleep(0.1)
 
-processes = []
-num_processes = os.cpu_count()
+threads = []
+num_threads = 10
 # print(num_processes)
 
 # create processes
-for i in range(num_processes):
-    p = Process(target=square_numbers)
-    processes.append(p)
+for i in range(num_threads):
+    t = Thread(target=square_numbers)
+    threads.append(t)
 
 # start processes
-if __name__ == '__main__':
-    freeze_support()
-    for p in processes:
-        p.start() 
-    
-    # join
-    for p in processes:
-        p.join()
-    
 
+for t in threads:
+    t.start() 
     
+# join
+for t in threads:
+    t.join()
+
+
 print('end main')
