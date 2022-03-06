94906936caf02a55bf69971f1ae6942822074052
anuragjanghala <42205669+anuragjanghala@users.noreply.github.com>
2022-03-06 20:47:44 +0000

    multithreading with que, lock and daemon thread

--- threading_examples.py
+++ threading_examples.py
@@ -1,36 +1,79 @@
-from threading import Thread, Lock
+from threading import Thread, Lock, current_thread
+from queue import Queue
 import time
 
-database_value = 0
+# database_value = 0
 
-def increase(lock):
-    global database_value
+# def increase(lock):
+#     global database_value
     
-    # lock will lock the state so it wont switch to thread2 and if we remove lock it will switch and answer will result to 1 instead of 2.
-    with lock:
-        #lock.acquire()
-        local_cpy = database_value
-        # processing
-        local_cpy += 1
-        time.sleep(0.1)
-        database_value = local_cpy
-        #lock.release()
+#     # lock will lock the state so it wont switch to thread2 and if we remove lock it will switch and answer will result to 1 instead of 2.
+#     with lock:
+#         #lock.acquire()
+#         local_cpy = database_value
+#         # processing
+#         local_cpy += 1
+#         time.sleep(0.1)
+#         database_value = local_cpy
+#         #lock.release()
 
+
+# if __name__ == '__main__':
+    
+#     lock = Lock()
+#     print('start value', database_value)
+    
+#     thread1 = Thread(target=increase, args=(lock,))
+#     thread2 = Thread(target=increase, args=(lock,))
+    
+#     thread1.start()
+#     thread2.start()
+    
+#     thread1.join()
+#     thread2.join()
+    
+#     print('end value', database_value)
+
+#     print('end main')
+
+
+##################################################################
+
+def worker(q,lock):
+    while True:
+        value = q.get()
+        
+        # processing
+        with lock:
+            print(f'in {current_thread().name} got {value}')
+        q.task_done()
 
 if __name__ == '__main__':
-    
+    q = Queue()
     lock = Lock()
-    print('start value', database_value)
+    # q.put(1)
+    # q.put(2)
+    # q.put(3)
     
-    thread1 = Thread(target=increase, args=(lock,))
-    thread2 = Thread(target=increase, args=(lock,))
+    # # 3 2 1 -->
+    # first = q.get()
+    # print(first)
     
-    thread1.start()
-    thread2.start()
     
-    thread1.join()
-    thread2.join()
+    # q.task_done()
+    # q.join()
     
-    print('end value', database_value)
-
+    num_threads = 10
+    
+    for i in range(num_threads):
+        thread = Thread(target=worker, args=(q,lock))
+        thread.daemon=True
+        thread.start()
+        
+        
+    for i in range(1, 21):
+        q.put(i)
+    
+    q.join()
+    
     print('end main')
