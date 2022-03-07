005dc8b63c807a8e95108a03b44a24b2f3256a7e
anuragjanghala <42205669+anuragjanghala@users.noreply.github.com>
2022-03-07 12:44:28 +0000

    multiprocessing using array for data exchange

--- multi.py
+++ multi.py
@@ -68,23 +68,24 @@ from multiprocessing import Process, Value, Array, Lock
 import time
 
 
-def add_100(number, lock):
+def add_100(numbers, lock):
     for i in range(100):
         time.sleep(0.01)
         
-        with lock:
-            number.value += 1
+        for i in range(len(numbers)):
+            with lock:
+                numbers[i] += 1
         
 
 if __name__ == '__main__':
     
     lock = Lock()
-    shared_number = Value('i', 0)
-    #shared_array = Array('d', [0.0, 100.0, 200.0])
-    print('Number at beginning is', shared_number.value)
+    #shared_number = Value('i', 0)
+    shared_array = Array('d', [0.0, 100.0, 200.0])
+    print('array at beginning is', shared_array[:])
     
-    p1= Process(target=add_100, args=(shared_number,lock))
-    p2 = Process(target=add_100, args=(shared_number,lock))
+    p1= Process(target=add_100, args=(shared_array,lock))
+    p2 = Process(target=add_100, args=(shared_array,lock))
     
     p1.start()
     p2.start()
@@ -92,4 +93,4 @@ if __name__ == '__main__':
     p1.join()
     p2.join()
     
-    print('number at the end is ', shared_number.value)
+    print('array at the end is ', shared_array[:])
