11eba0457ad2879e64e20c104cbd0bbcd472daf0
anuragjanghala <42205669+anuragjanghala@users.noreply.github.com>
2022-03-07 12:36:01 +0000

    multiprocessing sharing data within processes using value

--- multi.py
+++ multi.py
@@ -34,30 +34,62 @@
 
 
 ##################################################################
-from threading import Thread
+# from threading import Thread
+# import time
+
+# def square_numbers():
+#     for i in range(100):
+#         i*i
+#         time.sleep(0.1)
+
+# if __name__ == '__main__':
+#     threads = []
+#     num_threads = 10
+    
+#     for i in range(num_threads):
+#         t = Thread(target=square_numbers)
+#         threads.append(t)
+
+#     # start threads
+
+#     for t in threads:
+#         t.start() 
+        
+#     # join threads: wait for them to complete
+#     for t in threads:
+#         t.join()
+
+
+# print('end main')
+
+##################################################################
+
+from multiprocessing import Process, Value, Array, Lock
 import time
 
-def square_numbers():
+
+def add_100(number, lock):
     for i in range(100):
-        i*i
-        time.sleep(0.1)
+        time.sleep(0.01)
+        
+        with lock:
+            number.value += 1
+        
 
 if __name__ == '__main__':
-    threads = []
-    num_threads = 10
     
-    for i in range(num_threads):
-        t = Thread(target=square_numbers)
-        threads.append(t)
-
-    # start threads
-
-    for t in threads:
-        t.start() 
-        
-    # join threads: wait for them to complete
-    for t in threads:
-        t.join()
-
-
-print('end main')
+    lock = Lock()
+    shared_number = Value('i', 0)
+    #shared_array = Array('d', [0.0, 100.0, 200.0])
+    print('Number at beginning is', shared_number.value)
+    
+    p1= Process(target=add_100, args=(shared_number,lock))
+    p2 = Process(target=add_100, args=(shared_number,lock))
+    
+    p1.start()
+    p2.start()
+    
+    p1.join()
+    p2.join()
+    
+    print('number at the end is ', shared_number.value)
