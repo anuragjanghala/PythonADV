445c291c4dc869c2ececceedf5c30b2271f38b02
anuragjanghala <42205669+anuragjanghala@users.noreply.github.com>
2022-03-07 12:51:27 +0000

    multiprocessing with queue

--- multi.py
+++ multi.py
@@ -65,6 +65,7 @@
 ##################################################################
 
 from multiprocessing import Process, Value, Array, Lock
+from multiprocessing import Queue
 import time
 
 
@@ -77,15 +78,22 @@ def add_100(numbers, lock):
                 numbers[i] += 1
         
 
+def square(numbers, queue):
+    for i in numbers:
+        queue.put(i*i)
+        
+def make_negative(numbers,queue):
+    for i in numbers:
+        queue.put(-1*i)
+
 if __name__ == '__main__':
     
-    lock = Lock()
-    #shared_number = Value('i', 0)
-    shared_array = Array('d', [0.0, 100.0, 200.0])
-    print('array at beginning is', shared_array[:])
+    numbers = range(1,6)
+    q = Queue()
     
-    p1= Process(target=add_100, args=(shared_array,lock))
-    p2 = Process(target=add_100, args=(shared_array,lock))
+    p1 = Process(target=square, args=(numbers, q))
+    p2 = Process(target=make_negative, args=(numbers, q))
+    
     
     p1.start()
     p2.start()
@@ -93,4 +101,5 @@ if __name__ == '__main__':
     p1.join()
     p2.join()
     
-    print('array at the end is ', shared_array[:])
+    while not q.empty():
+        print(q.get())
