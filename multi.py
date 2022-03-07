07586f80ed68aa2b24b16759ed3792d5475dbc7c
anuragjanghala <42205669+anuragjanghala@users.noreply.github.com>
2022-03-07 12:58:13 +0000

    multiprocesssing with pool

--- multi.py
+++ multi.py
@@ -64,42 +64,64 @@
 
 ##################################################################
 
-from multiprocessing import Process, Value, Array, Lock
-from multiprocessing import Queue
-import time
+# from multiprocessing import Process, Value, Array, Lock
+# from multiprocessing import Queue
+# import time
 
 
-def add_100(numbers, lock):
-    for i in range(100):
-        time.sleep(0.01)
+# def add_100(numbers, lock):
+#     for i in range(100):
+#         time.sleep(0.01)
         
-        for i in range(len(numbers)):
-            with lock:
-                numbers[i] += 1
+#         for i in range(len(numbers)):
+#             with lock:
+#                 numbers[i] += 1
         
 
-def square(numbers, queue):
-    for i in numbers:
-        queue.put(i*i)
+# def square(numbers, queue):
+#     for i in numbers:
+#         queue.put(i*i)
         
-def make_negative(numbers,queue):
-    for i in numbers:
-        queue.put(-1*i)
+# def make_negative(numbers,queue):
+#     for i in numbers:
+#         queue.put(-1*i)
+
+# if __name__ == '__main__':
+    
+#     numbers = range(1,6)
+#     q = Queue()
+    
+#     p1 = Process(target=square, args=(numbers, q))
+#     p2 = Process(target=make_negative, args=(numbers, q))
+    
+    
+#     p1.start()
+#     p2.start()
+    
+#     p1.join()
+#     p2.join()
+    
+#     while not q.empty():
+#         print(q.get())
+
+
+##################################################################
+from multiprocessing import Pool
+
+
+def cube(number):
+    return number*number*number
 
 if __name__ == '__main__':
     
-    numbers = range(1,6)
-    q = Queue()
+    numbers= range(10)
+    pool = Pool()
     
-    p1 = Process(target=square, args=(numbers, q))
-    p2 = Process(target=make_negative, args=(numbers, q))
+    # map, apply, join, close
+    result = pool.map(cube, numbers)
+    # pool.apply(cube, numbers[0]) # this will execute the process with the pool once with one argument
     
+    pool.close()
+    pool.join()
     
-    p1.start()
-    p2.start()
-    
-    p1.join()
-    p2.join()
-    
-    while not q.empty():
-        print(q.get())
+    print(result)
