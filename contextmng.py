f42f8fa2666e4b0164491c73d1f95304205b16a5
anuragjanghala <42205669+anuragjanghala@users.noreply.github.com>
2022-03-08 20:16:10 +0000

    context management using generator and decorator

--- contextmng.py
+++ contextmng.py
@@ -23,28 +23,48 @@
 
 ##########################################
 
-class ManagedFile:
-    def __init__(self, fileName) -> None:
-        print('init')
-        self.fileName = fileName
+# class ManagedFile:
+#     def __init__(self, fileName) -> None:
+#         print('init')
+#         self.fileName = fileName
 
-    def __enter__(self):
-        print('Enter')
-        self.file = open(self.fileName,'w')
-        return self.file
+#     def __enter__(self):
+#         print('Enter')
+#         self.file = open(self.fileName,'w')
+#         return self.file
     
-    def __exit__(self, exc_type, exc_value, exc_traceback):
-        if self.file:
-            self.file.close()
-        if exc_type is not None:
-            print('exception has been handled')
-        #print('exc:', exc_type, exc_value)
-        print('exit')
-        return True
+#     def __exit__(self, exc_type, exc_value, exc_traceback):
+#         if self.file:
+#             self.file.close()
+#         if exc_type is not None:
+#             print('exception has been handled')
+#         #print('exc:', exc_type, exc_value)
+#         print('exit')
+#         return True
 
 
-with ManagedFile('notes.txt') as file:
-    print('do some stuff...')
-    file.write('some todo...')
-    file.somemethod() # using this to create exception
-print('continuing')
+# with ManagedFile('notes.txt') as file:
+#     print('do some stuff...')
+#     file.write('some todo...')
+#     file.somemethod() # using this to create exception
+# print('continuing')
+
+
+################################################
+# context management using methods
+
+from contextlib import contextmanager
+from typing import final
+
+
+@contextmanager
+def open_managed_file(filename):
+    f = open(filename, 'w')
+    try:
+        yield f # generator
+    finally:
+        f.close()
+
+
+with open_managed_file('notes.txt') as f:
+    f.write('some todoooo...')
